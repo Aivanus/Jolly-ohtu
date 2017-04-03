f621bfae9de772cc11166c5b6e16f8be3587c2f7
arkuar <arttukuronen@gmail.com>
2017-04-03 18:07:46 +0300

    Testi ja korjattu riippuvuus

---
+++ src/test/java/JollyOhtu/Objects/BookTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JollyOhtu.Objects;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Arttu
+ */
+public class BookTest {
+    
+    public BookTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void bookIsCreatedCorrectly() {
+        Book book = new Book("Pekka", "Testauskirja", "Julkaisija", 2017);
+        assertEquals("Book: author='Pekka', title='Testauskirja', publisher='Julkaisija', year='2017'", book.toString());
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
