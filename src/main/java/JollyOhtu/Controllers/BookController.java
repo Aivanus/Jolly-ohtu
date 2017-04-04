b88dacaa639df163e5825760fb35303ea43acc05
arkuar <arttukuronen@gmail.com>
2017-04-04 21:10:25 +0300

    Proceedings added

--- src/main/java/JollyOhtu/Controllers/BookController.java
+++ src/main/java/JollyOhtu/Controllers/BookController.java
@@ -10,21 +10,15 @@ package JollyOhtu.Controllers;
  * @author alsu
  */
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class BookController{
-    @RequestMapping(value="/add_book", method=GET)
+public class BookController {
+
+    @RequestMapping(value = "/add_book", method = GET)
     public String addBook() {
         return "add_book";
     }
-    
-   
-
 
 }
--- src/main/java/JollyOhtu/Objects/Proceeding.java
+++ src/main/java/JollyOhtu/Objects/Proceeding.java
@@ -1,6 +1,5 @@
 package JollyOhtu.Objects;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -9,39 +8,31 @@ import javax.persistence.Id;
  *
  * @author Arttu
  */
-@Entity
-public class Book {
-
-    public Book() {
-    }
+public class Proceeding {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String author;
     private String title;
-    private String publisher;
     private int year;
 
     //Optional fields
+    private String editor;
     private int volume;
     private String series;
     private String address;
-    private String edition;
     private int month;
+    private String publisher;
+    private String organization;
     private String note;
 
-    public Book(String author, String title, String publisher, Integer year) {
-        this.author = author;
+    public Proceeding(String title, int year) {
         this.title = title;
-        this.publisher = publisher;
         this.year = year;
     }
 
     @Override
     public String toString() {
-        return String.format("Book: author='%s', title='%s', publisher='%s', year='%d'",
-                author, title, publisher, year);
+        return "Proceeding: title=" + title + " year=" + year;
     }
-
 }
