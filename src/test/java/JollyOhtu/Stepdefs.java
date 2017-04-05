e94df00175d9eff405c7795891eb37d7746da514
Ivan Kropotov <ivank1995@hotmail.com>
2017-04-05 20:58:41 +0300

    trying to get selenium with cucumber to work

--- src/test/java/JollyOhtu/Stepdefs.java
+++ src/test/java/JollyOhtu/Stepdefs.java
@@ -5,13 +5,25 @@ import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 public class Stepdefs {
+    
+//    WebDriver driver = new ChromeDriver();
+    WebDriver driver = new HtmlUnitDriver();
+//    String baseUrl = "http://localhost:8080";
+    
     @Given("^Add book view is selected$")
     public void add_book_view_is_selected() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+//        driver.get(baseUrl);
+//        System.out.println(driver.getPageSource());
+//        WebElement element = driver.findElement(By.linkText("Add Book"));
+//        element.click();
+//        System.out.println(driver.getPageSource());
     }
 
     @When("^Valid book author \"([^\"]*)\", title \"([^\"]*)\", year (\\d+) and publisher \"([^\"]*)\" are given$")
