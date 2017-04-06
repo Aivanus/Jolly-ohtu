ee2b06beecd4363fe8c448943a498aad5aadde27
Aivanus <ivank1995@hotmail.com>
2017-04-06 13:47:10 +0300

    even more trying to get selenium working

--- src/main/java/JollyOhtu/Tester.java
+++ src/main/java/JollyOhtu/Tester.java
@@ -9,22 +9,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Tester {
-    public static void main(String[] args) {
-//        SpringApplication.run(Main.class, args);
-        
+    public static void main(String[] args) {      
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ivan\\Documents\\GitHub\\Jolly-ohtu\\chromedriver.exe");
-
+        
+        
         WebDriver driver = new ChromeDriver();
 
 
-        driver.get("http://localhost:8080");
+        driver.get("http://www.google.com/");
         
         try {
             Thread.sleep(4 * 1000);
         } catch (Exception e) {
         }
+        
+        driver.quit();
     }
 }
