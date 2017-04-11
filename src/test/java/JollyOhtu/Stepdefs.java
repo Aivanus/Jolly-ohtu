b8fa97450c6852bafadb5fd176be8053cbb99432
Aivanus <ivank1995@hotmail.com>
2017-04-11 17:05:46 +0300

    changed webdriver to headless for travis

--- src/test/java/JollyOhtu/Stepdefs.java
+++ src/test/java/JollyOhtu/Stepdefs.java
@@ -11,17 +11,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = Main.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@ContextConfiguration(classes = Main.class)
+//@SpringBootTest
 public class Stepdefs {
 
-
-    private WebDriver driver = new ChromeDriver();
+    private WebDriver driver = new HtmlUnitDriver();
     String baseUrl = "https://sheltered-gorge-31986.herokuapp.com/";
     
     @Test
