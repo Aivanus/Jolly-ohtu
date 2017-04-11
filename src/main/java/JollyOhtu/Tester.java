90973a11f57ece93f46da18ce543df1e80a0f839
Aivanus <ivank1995@hotmail.com>
2017-04-11 15:52:18 +0300

    Finally got Selenium to boot

--- src/main/java/JollyOhtu/Tester.java
+++ src/main/java/JollyOhtu/Tester.java
@@ -1,18 +1,9 @@
 
 package JollyOhtu;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Tester {
--- src/test/java/JollyOhtu/Stepdefs.java
+++ src/test/java/JollyOhtu/Stepdefs.java
@@ -1,40 +1,38 @@
-
 package JollyOhtu;
 
 import cucumber.api.PendingException;
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
-@ContextConfiguration
+@ContextConfiguration(classes = Main.class)
+@SpringBootTest
 public class Stepdefs {
+
+
+    private WebDriver driver = new ChromeDriver();
+    String baseUrl = "https://sheltered-gorge-31986.herokuapp.com/";
     
-    @Autowired
-    private TestRestTemplate restTemplate;
-    
-    private ResponseEntity<String> response;
-    
+
     @Given("^Add book view is selected$")
     public void add_book_view_is_selected() throws Throwable {
-         response = restTemplate.exchange("/add_book", HttpMethod.GET, null, String.class);
-         System.out.println(response.toString());
-         
+        driver.get(baseUrl);
+        Thread.sleep(10000);
+        
+        driver.quit();
+
     }
 
     @When("^Valid book author \"([^\"]*)\", title \"([^\"]*)\", year (\\d+) and publisher \"([^\"]*)\" are given$")
     public void valid_book_author_title_year_and_publisher_are_given(String author, String title, int year, String publisher) throws Throwable {
-        response = restTemplate.postForEntity("add_book", response, String.class);
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
@@ -45,5 +43,8 @@ public class Stepdefs {
         throw new PendingException();
     }
 
-
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 }
