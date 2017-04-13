a7db0ffd60486c341b227e9d5e5b5e0e90373a13
Aivanus <ivank1995@hotmail.com>
2017-04-13 19:02:42 +0300

    Additional fields in books tested with valid values

--- src/test/java/JollyOhtu/Stepdefs.java
+++ src/test/java/JollyOhtu/Stepdefs.java
@@ -1,12 +1,15 @@
 package JollyOhtu;
 
 import cucumber.api.DataTable;
+import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import static org.junit.Assert.assertTrue;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -54,8 +57,26 @@ public class Stepdefs {
         driver.findElement(By.id("author")).sendKeys(table.get(0).get(1));
         driver.findElement(By.id("title")).sendKeys(table.get(1).get(1));
         driver.findElement(By.id("year")).sendKeys(table.get(2).get(1));
-        driver.findElement(By.id("publisher")).sendKeys(table.get(3).get(1));
-        // Click submit
+        driver.findElement(By.id("publisher")).sendKeys(table.get(3).get(1));   
+    }
+    
+     @When("^Optional fields are chosen$")
+    public void optional_fields_are_chosen() throws Throwable {
+        driver.findElement(By.xpath("//button[contains(.,'Additional fields')]")).click();
+    }
+
+    @When("^Valid optional field information is entered:$")
+    public void valid_optional_field_information_is_entered(List<List<String>> table) throws Throwable {
+        driver.findElement(By.id("volume")).sendKeys(table.get(0).get(1));
+        driver.findElement(By.id("series")).sendKeys(table.get(1).get(1));
+        driver.findElement(By.id("address")).sendKeys(table.get(2).get(1));
+        driver.findElement(By.id("edition")).sendKeys(table.get(3).get(1));
+        driver.findElement(By.id("month")).sendKeys(table.get(4).get(1));
+        driver.findElement(By.id("note")).sendKeys(table.get(5).get(1));
+    }
+
+    @When("^User presses button Add$")
+    public void user_presses_button_add() throws Throwable {
         driver.findElement(By.xpath("//button[contains(.,'Add')]")).submit();
     }
 
@@ -72,5 +93,13 @@ public class Stepdefs {
     // Helper methods
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
+    }
+    
+    private void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Stepdefs.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- src/test/resources/JollyOhtu/addBooks.feature
+++ src/test/resources/JollyOhtu/addBooks.feature
@@ -1,10 +1,30 @@
 Feature: As a user I want to be able to add book references
 
-  Scenario: User can create a book reference with valid mandatory fields
+  Background: 
     Given Add book view is selected
+
+  Scenario: User can create a book reference with valid mandatory fields
     When Valid mandatory book information is entered:
       | author    | Jonh Johnman     |
       | title     | Interesting Book |
       | year      | 1984             |
       | publisher | PaperPress       |
+    And User presses button Add
+    Then new book reference is created
+
+  Scenario: User can create a book reference with valid mandatory and optional fields
+    When Valid mandatory book information is entered:
+      | author    | Jonh Johnman     |
+      | title     | Interesting Book |
+      | year      | 1984             |
+      | publisher | PaperPress       |
+    And Optional fields are chosen
+    And Valid optional field information is entered:
+      | volume  | 1                |
+      | series  | 12               |
+      | address | Bakerstreet 1337 |
+      | edition | 42               |
+      | month   | 5                |
+      | note    | smtn             |
+    And User presses button Add
     Then new book reference is created
