b47a6ecfff3437b6774cc3e9e628ac56ab063731
Aivanus <ivank1995@hotmail.com>
2017-04-13 16:49:50 +0300

    fixed wrong webdriver, wich caused travis to error

--- src/test/java/JollyOhtu/Stepdefs.java
+++ src/test/java/JollyOhtu/Stepdefs.java
@@ -5,11 +5,10 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import static org.junit.Assert.assertTrue;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 //@RunWith(SpringRunner.class)
 //@ContextConfiguration(classes = Main.class)
