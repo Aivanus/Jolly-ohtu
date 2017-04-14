0c4c448b991e44031f2b43cc1c07096bd3058f2d
Topi Kiviluoma <topi.kiviluoma@helsinki.fi>
2017-04-14 12:47:03 +0300

    view & controller for reference list

---
+++ src/main/java/JollyOhtu/Controllers/ReferenceController.java
@@ -0,0 +1,41 @@
+package JollyOhtu.Controllers;
+
+import JollyOhtu.Repository.ArticleRepository;
+import JollyOhtu.Repository.BookRepository;
+import JollyOhtu.Repository.InproceedingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ * Created by topi on 14.4.2017.
+ */
+
+@Controller
+public class ReferenceController {
+    @Autowired
+    private ArticleRepository artRepo;
+    @Autowired
+    private BookRepository bookRepo;
+    @Autowired
+    private InproceedingsRepository inproRepo;
+    private List<Object> references = new ArrayList<>();
+
+    @RequestMapping(value = "/list_references", method = GET)
+    public String listReferences(Model model, String reference) {
+        model.addAttribute("reference", reference);
+
+        return "list_references";
+    }
+
+    public void getReferences() {
+
+    }
+
+}
