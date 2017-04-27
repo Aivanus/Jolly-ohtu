512ba879bc293105623c60523086e15df34e4992
Ivan Kropotov <ivank1995@hotmail.com>
2017-04-27 16:41:26 +0300

    started working on acceptance tests for delete reference story

--- src/test/java/JollyOhtu/Stepdefs.java
+++ src/test/java/JollyOhtu/Stepdefs.java
@@ -1,10 +1,15 @@
 package JollyOhtu;
 
+import JollyOhtu.Objects.Book;
+import JollyOhtu.Repository.ArticleRepository;
+import JollyOhtu.Repository.BookRepository;
+import JollyOhtu.Repository.InproceedingsRepository;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,25 +19,33 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 
 //@RunWith(SpringRunner.class)
 //@ContextConfiguration(classes = Main.class)
 //@SpringBootTest
 public class Stepdefs {
     
+    @Autowired
+    private ArticleRepository artRepo;
+    @Autowired
+    private BookRepository bookRepo;
+    @Autowired
+    private InproceedingsRepository inproRepo;
+
     private WebDriver driver = null;
     String baseUrl = "http://localhost:8080/";
-    
+
     @Before
     public void setUp() {
 
         if (System.getProperty("os.name").startsWith("Windows")) {
-            driver = new ChromeDriver();
+            driver = new HtmlUnitDriver();
+//            driver = new ChromeDriver();
         } else {
             driver = new FirefoxDriver();
         }
-
-//        driver = new HtmlUnitDriver();  //for testing purposes
 
     }
 
@@ -50,25 +63,25 @@ public class Stepdefs {
         driver.get(baseUrl);
         driver.findElement(By.linkText("Add Book")).click();
     }
-    
+
     @Given("^Add article view is selected$")
     public void add_article_view_is_selected() throws Throwable {
         driver.get(baseUrl);
         driver.findElement(By.linkText("Add Article")).click();
     }
-    
+
     @Given("^Add inproceedings view is selected$")
     public void add_inproceedings_view_is_selected() throws Throwable {
         driver.get(baseUrl);
         driver.findElement(By.linkText("Add Inproceedings")).click();
     }
-    
+
     @Given("^List view is selected$")
     public void list_view_is_selected() throws Throwable {
         driver.get(baseUrl);
         driver.findElement(By.linkText("List References")).click();
     }
-    
+
     @Given("^Book reference is successfully created with following fields:$")
     public void book_reference_is_successfully_created_with_following_fields(List<List<String>> table) throws Throwable {
         add_book_view_is_selected();
@@ -77,7 +90,7 @@ public class Stepdefs {
         user_presses_button_add();
         message_is_presented("Reference was saved successfully!");
     }
-    
+
     @Given("^Book reference with author \"([^\"]*)\" is not created$")
     public void book_reference_with_author_is_not_created(String author) throws Throwable {
         add_book_view_is_selected();
@@ -85,7 +98,7 @@ public class Stepdefs {
         user_presses_button_add();
         message_is_presented("You must fill in the fields marked by *");
     }
-    
+
     @Given("^Article reference is successfully created with following fields:$")
     public void article_reference_is_successfully_created_with_following_fields(List<List<String>> table) throws Throwable {
         add_article_view_is_selected();
@@ -94,7 +107,7 @@ public class Stepdefs {
         user_presses_button_add();
         message_is_presented("Reference was saved successfully!");
     }
-    
+
     @Given("^Article reference with author \"([^\"]*)\" is not created$")
     public void article_reference_with_author_is_not_created(String author) throws Throwable {
         add_book_view_is_selected();
@@ -102,7 +115,7 @@ public class Stepdefs {
         user_presses_button_add();
         message_is_presented("You must fill in the fields marked by *");
     }
-    
+
     @Given("^Inproceedings reference is successfully created with following fields:$")
     public void inproceedings_reference_is_successfully_created_with_following_fields(List<List<String>> table) throws Throwable {
         add_inproceedings_view_is_selected();
@@ -111,7 +124,7 @@ public class Stepdefs {
         user_presses_button_add();
         message_is_presented("Reference was saved successfully!");
     }
-    
+
     @Given("^Inproceedings reference with author \"([^\"]*)\" is not created$")
     public void inproceedings_reference_with_author_is_not_created(String author) throws Throwable {
         add_inproceedings_view_is_selected();
@@ -119,7 +132,7 @@ public class Stepdefs {
         user_presses_button_add();
         message_is_presented("You must fill in the fields marked by *");
     }
-    
+
     @Given("^Download file view has been selected$")
     public void download_file_view_has_been_selected() throws Throwable {
         driver.get(baseUrl);
@@ -140,82 +153,93 @@ public class Stepdefs {
     public void valid_mandatory_book_information_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^No mandatory book information is entered:$")
     public void no_mandatory_book_information_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^Optional fields are chosen$")
     public void optional_fields_are_chosen() throws Throwable {
         driver.findElement(By.xpath("//button[contains(.,'Additional fields')]")).click();
     }
-    
+
     @When("^Valid optional field information is entered:$")
     public void valid_optional_field_information_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^Invalid optional field information is entered:$")
     public void invalid_optional_field_information_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^Mandatory book information with only author and title is entered:$")
     public void mandatory_book_information_with_only_author_and_title_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^Valid mandatory article information is entered:$")
     public void valid_mandatory_article_information_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^No mandatory article information is entered:$")
     public void no_mandatory_article_information_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^User presses button Add$")
     public void user_presses_button_add() throws Throwable {
         driver.findElement(By.xpath("//button[contains(.,'Add')]")).submit();
     }
-    
+
     @When("^Mandatory article information with only author and title is entered:$")
     public void mandatory_article_information_with_only_author_and_title_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^Mandatory inproceedings information with only author and title is entered:$")
     public void mandatory_inproceedings_information_with_only_author_and_title_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^Valid mandatory inproceedings information is entered:$")
     public void valid_mandatory_inproceedings_information_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("File name is entered:$")
     public void file_name_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^No mandatory inproceedings information is entered:$")
     public void no_mandatory_inproceedings_information_is_entered(List<List<String>> table) throws Throwable {
         enterValuesById(table);
     }
-    
+
     @When("^File name \"([^\"]*)\" is entered$")
     public void file_name_is_entered(String name) throws Throwable {
         driver.findElement(By.name("name")).sendKeys(name);
-        
+
     }
-    
+
     @When("^I press Generate file button")
     public void user_presses_button_generate_file() throws Throwable {
         driver.findElement(By.xpath("//button[contains(.,'Generate file')]")).submit();
     }
+
+    @When("^Checkbox next to the book with title \"([^\"]*)\" is selected$")
+    public void checkbox_next_to_the_book_is_selected(String title) throws Throwable {
+        List<Book> list = bookRepo.findByTitle(title);
+         long id = list.get(0).getId();
+         System.out.println(id);
+         sleep(2000);
+         driver.findElement(By.xpath("//input[@type='checkbox'][@name='del_books'][@value="+id+"]")).click();
+        sleep(2000);
+    }
+
 
     /*
     
@@ -230,40 +254,40 @@ public class Stepdefs {
     public void message_is_presented(String message) throws Throwable {
         pageHasContent(message);
     }
-    
+
     @Then("^List view is shown$")
     public void list_view_is_shown() throws Throwable {
         pageHasContent("Books");
         pageHasContent("Articles");
         pageHasContent("Inproceedings");
     }
-    
+
     @Then("^Book reference row should be visible with following fields:$")
     public void book_reference_row_should_be_visible_with_following_fields(List<String> list) throws Throwable {
         for (String string : list) {
             pageHasContent(string);
         }
     }
-    
+
     @Then("^Article reference row should be visible with following fields:$")
     public void article_reference_row_should_be_visible_with_following_fields(List<String> list) throws Throwable {
         for (String string : list) {
             pageHasContent(string);
         }
     }
-    
+
     @Then("^Inproceedings reference row should be visible with following fields:$")
     public void inproceedings_reference_row_should_be_visible_with_following_fields(List<String> list) throws Throwable {
         for (String string : list) {
             pageHasContent(string);
         }
     }
-    
+
     @Then("^Row with author \"([^\"]*)\" should not be visible$")
     public void row_with_author_should_not_be_visible(String author) throws Throwable {
         pageDoesntHaveContent(author);
     }
-    
+
     @Then("^User is on download page$")
     public void user_is_on_download_page() throws Throwable {
         pageHasContent("File name");
@@ -288,11 +312,11 @@ public class Stepdefs {
     private void pageHasContent(String content) {
         assertTrue(driver.getPageSource().contains(content));
     }
-    
+
     private void pageDoesntHaveContent(String content) {
         assertFalse(driver.getPageSource().contains(content));
     }
-    
+
     private void enterValuesById(List<List<String>> table) {
         for (List<String> table1 : table) {
             driver.findElement(By.id(table1.get(0))).sendKeys(table1.get(1));
---
+++ src/test/resources/JollyOhtu/deleteReference.feature
@@ -0,0 +1,21 @@
+Feature: User can delete references
+
+  Scenario: User can delete a book reference
+    Given Book reference is successfully created with following fields:
+      | author    | DeleteThis1          |
+      | title     | InterestingBook76234 |
+      | publisher | PaperPresser23       |
+      | year      | 1977                 |
+      | callId    | callID3DELBOOK       |
+      | volume    | 11                   |
+      | series    | 1                    |
+      | address   | Bakerstreet13asdf    |
+      | edition   | 52                   |
+      | month     | 1                    |
+      | note      | DeleteOneBookTest    |
+      When List view is selected
+      And Checkbox next to the book with title "InterestingBook76234" is selected
+And User presses button Delete Selected
+And User confirms action Delete
+Then Message "One book reference was deleted succesfully." is presented
+But Row with ID "callID3DELBOOK" should not be visible
