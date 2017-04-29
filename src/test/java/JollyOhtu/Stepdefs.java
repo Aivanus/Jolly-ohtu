993f2c9f7fb1f9d0920a7ee8bf1fa4e0f2cf20a0
Topi Kiviluoma <topi.kiviluoma@helsinki.fi>
2017-04-29 18:29:12 +0300

    Changed driver back from test mode

--- src/test/java/JollyOhtu/Stepdefs.java
+++ src/test/java/JollyOhtu/Stepdefs.java
@@ -51,9 +51,9 @@ public class Stepdefs {
             driver = new HtmlUnitDriver();
 //                        driver = new ChromeDriver();
         } else {
-            driver = new HtmlUnitDriver();
+            //driver = new HtmlUnitDriver();
 
-            //driver = new FirefoxDriver();
+            driver = new FirefoxDriver();
         }
 
     }
